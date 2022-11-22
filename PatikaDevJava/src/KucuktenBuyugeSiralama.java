14103d10241b8192a3edda3fe94a8bc44bb029f4
ONUR CAN AKKOYUN <akkoyunonurcan@gmail.com>
2022-11-22 21:06:32 +0300

    Update and rename BuyuktenKucugeSiralama.java to KucuktenBuyugeSiralama.java

--- PatikaDevJava/src/KucuktenBuyugeSiralama.java
+++ PatikaDevJava/src/KucuktenBuyugeSiralama.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BuyuktenKucugeSiralama {
+public class KucuktenBuyugeSiralama {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
